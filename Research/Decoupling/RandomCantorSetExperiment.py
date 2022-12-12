afb94e946ef656f01c20f8b6d353ef752bc5dce4
Jacob Denson <jacob.denson3@googlemail.com>
2022-12-11 22:13:08 -0600

    Update

--- Research/Decoupling/RandomCantorSetExperiment.py
+++ Research/Decoupling/RandomCantorSetExperiment.py
@@ -59,69 +59,115 @@ def generate_centers(scale):
 		yield total
 
 def graph_oscillation(scale):
-	# Graph up to 1/3^i
+	# get a matrix whose centers consist of all 2^i centers
+	# of a Cantor set discretized to a scale 1/3^i
 	centers = np.matrix(list(generate_centers(scale)))
-#	centers = np.asmatrix(list(generate_centers(scale)), float))
 
-	print("calculate centers")
+	# uncomment this to get a matrix whose centers
+	# are uniformly distributed between [0,1]
+	#centers = np.matrix(np.random.random(2**scale))
 
+	print("finished calculating centers")
+
+	# test exponential sum against all integer
+	# values of x between -3^i and 3^i
 	x_values = np.asmatrix(np.arange(- 3**scale, 3**scale))
 
-#	print(np.shape(np.transpose(centers)))
-
+	# build a matrix consisting of all values of
+	# exp(2 pi i xi x), where x ranges over all x-values,
+	# and xi ranges over all centers of the Cantor set.
 	exponents = np.exp(2*np.pi*1j*np.transpose(centers)*x_values)
-#	print(np.shape(exponents))
 
-	print("computed exponents")
+	print("finished calculating exponentials")
 
+	# use matrix algebra to compute the exponential sums
+	# sum_xi exp( 2 pi i xi x ) for each value x.
 	y_ones = np.ones((1,2**scale))
-#	print(np.shape(y_ones))
+	exponential_sums = y_ones*exponents
 
-	y_values = y_ones*exponents
+	print("finished calculating exponential sums")
 
-#	print(x_values.A1)
-#	print(y_values.A1)
+	# get the y-values of the horizontal line y = 2^(i/2).
+	cancellation_flat_line = (2**(0.5*scale))*np.ones(2*(3**scale))
 
-	# Plot the square root on y axis
-	y_flat_line = (2**(0.5*scale))*np.ones(2*(3**scale))
-	y_big_line = (2**scale)*np.ones(2*(3**scale))
+	# get the y-values of the horizontal line y = 2^i.
+	no_cancellation_flat_line = (2**scale)*np.ones(2*(3**scale))
 
-	plt.title("Test")
+	# get the values for the vertical lines x = -1
+	negative_x_line = (-1.0)*np.ones(2**(scale + 1))
+	positive_x_line = (+1.0)*np.ones(2**(scale + 1))
+	all_y_values = np.arange(0, 2**(scale+1))
+
+	# get values for y = 2**(i/2) 3**(i/2) / (|x| + 1)^{1/2}
+	y_fit_line = (2**(0.5*scale))*(3**(0.5*scale))*np.power(np.absolute(x_values) + 1,-0.5)
+
+	# get values for y = 2**(i/2) / (|x| + 1)
+	y_fit_line_2 = (2**(0.5*scale))*np.power(1.0*np.absolute(x_values) + 1.0,-1)
+
+
+
+
 
 	f1 = plt.figure(1)
-#	plt.axis([-100,100, 0, 2**scale])
-	plt.axis([-3**scale, 3**scale, 0, 2**scale])
 
-	plt.plot(x_values.A1,y_values.real.A1)
-	plt.plot(x_values.A1, y_flat_line, color="red", linewidth=2)
-	plt.plot(x_values.A1, y_big_line, color="red", linewidth=2)
+	plt.title("Real Part of Exponential Sum")
+	plt.axis([-3**scale, 3**scale, -2**scale, 2**scale])
 
-	all_y_values = np.arange(0, 2**(scale+1))
-	plt.plot(-(1.5**scale)*np.ones(2**(scale+1)), all_y_values, color="red", linewidth=2)
-	plt.plot(+(1.5**scale)*np.ones(2**(scale+1)), all_y_values, color = "red", linewidth=2)
+	# Graph y = Re [ sum exp (2 pi i xi x) ]
+	plt.plot(x_values.A1, exponential_sums.real.A1)
 
-	# Plot y = 2^{i/2} 3^{i/2} / x^{1/2}
-	y_fit_line = (2**(0.5*scale))*(3**(0.5*scale))*np.power(np.absolute(x_values) + 1,-0.5)
-	plt.plot(x_values.A1,y_fit_line.A1, color="red", linewidth=2)
+	# Graph y = 2^i
+	plt.plot(x_values.A1, no_cancellation_flat_line, color="red", linewidth=2)
 
-	# H <= 2^{i/2} 3^{i/2} / X^{1/2}
+	# Graph y = 2^{i/2}
+	plt.plot(x_values.A1, cancellation_flat_line, color="red", linewidth=2)
+
+	# Graph x = -1
+	plt.plot(negative_x_line, all_y_values, color="red", linewidth=2)
+
+	# Graph x = +1
+	plt.plot(positive_x_line, all_y_values, color = "red", linewidth=2)
+
+	# Plot y = 2^{i/2} 3^{i/2} / <x>^{1/2}
+	plt.plot(x_values.A1, y_fit_line.A1, color="red", linewidth=2)
+
+	# Plot y = 2^{i/2} / <x>
+	plt.plot(x_values.A1, y_fit_line_2.A1, color="green", linewidth=2)
+
+
+
+
 
 	f2 = plt.figure(2)
-#	plt.axis([-100,100, 0, 2**scale])
-	plt.axis([-3**scale, 3**scale, 0, 2**scale])
 
-	plt.plot(x_values.A1,y_values.imag.A1)
-	plt.plot(x_values.A1, y_flat_line, color="red", linewidth=2)
-	plt.plot(x_values.A1, y_big_line, color="red", linewidth=2)
+	plt.title("Imaginary Part of Exponential Sum")
+	plt.axis([-3**scale, 3**scale, -2**scale, 2**scale])
 
-	all_y_values = np.arange(0, 2**(scale+1))
-	plt.plot(-(1.5**scale)*np.ones(2**(scale+1)), all_y_values, color="red", linewidth=2)
-	plt.plot(+(1.5**scale)*np.ones(2**(scale+1)), all_y_values, color = "red", linewidth=2)
+	# Graph y = Im [ sum exp (2 pi i xi x) ]
+	plt.plot(x_values.A1, exponential_sums.imag.A1)
 
-	# Plot y = 2^{i/2} 3^{i/2} / x^{1/2}
-	# So should reach y = 2^{i/2} when x = 3^i
-	plt.plot(x_values.A1,y_fit_line.A1, color="red", linewidth=2)
+	# Graph y = 2^i
+	plt.plot(x_values.A1, no_cancellation_flat_line, color="red", linewidth=2)
+
+	# Graph y = 2^{i/2}
+	plt.plot(x_values.A1, cancellation_flat_line, color="red", linewidth=2)
+
+	# Graph x = -1
+	plt.plot(negative_x_line, all_y_values, color="red", linewidth=2)
+
+	# Graph x = +1
+	plt.plot(positive_x_line, all_y_values, color = "red", linewidth=2)
+
+	# Plot y = 2^{i/2} 3^{i/2} / <x>^{1/2}
+	plt.plot(x_values.A1, y_fit_line.A1, color="red", linewidth=2)
+
+	# Graph y = 2^{i/2} / <x>
+	plt.plot(x_values.A1, y_fit_line_2.A1, color="green", linewidth=2)
+
+
+
+
 
 	plt.show()
 
-graph_oscillation(10)
+graph_oscillation(8)
